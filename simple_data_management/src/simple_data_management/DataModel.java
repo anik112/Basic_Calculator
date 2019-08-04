bc63c7f03010e35df1a3abb7d05b16d43ad394c9
anik112 <paulanik112@gmail.com>
2019-08-04 11:09:55 +0600

    Update file 04/08/2019

---
+++ simple_data_management/src/simple_data_management/DataModel.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simple_data_management;
+
+/**
+ *
+ * @author Vista Soft IT
+ */
+public class DataModel {
+    
+    private int id;
+    private String name;
+    private String fatherName;
+    private String motherName;
+    private int age;
+    private String nidNumber;
+    private String mobile;
+    private String email;
+    private String address;
+
+    public DataModel() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public String getMotherName() {
+        return motherName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getNidNumber() {
+        return nidNumber;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public void setMotherName(String motherName) {
+        this.motherName = motherName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setNidNumber(String nidNumber) {
+        this.nidNumber = nidNumber;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
+    
+    
+}
---
+++ simple_data_management/src/simple_data_management/Simple_data_management.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simple_data_management;
+
+/**
+ *
+ * @author Vista Soft IT
+ */
+public class Simple_data_management {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
